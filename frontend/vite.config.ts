6755c14d0771b02cefb4f8dcdb243eaa84d82af2
Suphakit P <me@suphakit.net>
2023-08-31 05:00:47 +0700

    feat<config>: added proxy server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			"/list_post": {
+				target: "http://localhost:4201/be/",
+        changeOrigin: true,
+        secure: false,
+			},
+			"/rss" : {
+				target: "http://localhost:4201/",
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
 });
