722cc55355901d1cbe7de5bb77b8996cf339d47c
Suphakit P <me@suphakit.net>
2023-08-31 15:02:49 +0700

    chore<module>: seting up interface

---
+++ frontend/src/interface/blogPost.ts
@@ -0,0 +1,18 @@
+export interface PostInterface {
+  slug: string
+  title: string,
+  tags: string,
+  date: string,
+  author: string,
+  isPublish: boolean,
+  content: string
+}
+
+export interface VanishedInterface {
+  slug: string
+  title: string,
+  tags: string,
+  date: string,
+  author: string,
+  content: string
+}
